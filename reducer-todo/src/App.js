6426b3bf3e06790cb862bff167dab890ca5f5b8a
Jose Montero Jr <josemariodev@gmail.com>
2019-07-23 15:08:25 -0500

    components created and imported in App

--- reducer-todo/src/App.js
+++ reducer-todo/src/App.js
@@ -1,11 +1,40 @@
-import React from 'react';
+import React, { useReducer } from 'react';
 
-function App() {
+import { reducer, initialState } from './reducers/reducer';
+
+import Todos from './components/Todos';
+import Form from './components/Form';
+
+
+const App = () => {
+  const [state, dispatch] = useReducer(reducer, initialState);
+
+  const addItem = (e, item) => {
+    e.preventDefault();
+    dispatch({ type: 'ADD_TODO', payload: item });
+  };
+
+  const toggleTodo = itemid => {
+    dispatch({ type: 'TOGGLE_TODO', payload: itemid });
+  };
+
+  const clearCompleted = e => {
+    e.preventDefault();
+    dispatch({ type: 'CLEAR_COMPLETED' });
+  };
+
   return (
     <div className='App'>
-      <h1>test</h1>
+      <div className='header'>
+        <h1>Todo List</h1>
+        <Form addItem={addItem} />
+      </div>
+      <Todos
+        todos={state.todos}
+        toggleTodo={toggleTodo}
+        clearCompleted={clearCompleted}
+      />
     </div>
   );
-}
-
+};
 export default App;
